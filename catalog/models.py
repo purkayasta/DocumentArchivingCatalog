d23418879fa4ffffd51ee4f93dadd054e396fb46
purkayasta <pritompurkayasta@gmail.com>
2019-07-02 02:57:56 +0600

    created model and relevant test cases

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=120, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"self.name"
+
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=120, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"self.name"
+
+
+class Author(AbstractUser):
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return f"self.name"
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=120, blank=True)
+    file_path = models.FilePathField()
+    file_type = models.CharField(max_length=20)
+    keywords = models.ManyToManyField(Keyword)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    word_art = models.FilePathField()
+    contents = models.CharField(max_length=255)
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"self.name"
--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Tag
 
-# Create your tests here.
+
+class TagTestCase(TestCase):
+    def setUp(self):
+        Tag.objects.create(name="lion")
+        Tag.objects.create(name="cat")
+
+    def test_tag_exists(self):
+        """Searching with tag"""
+        result = Tag.objects.get(name='lion')
+        self.assertEqual(result.name, 'lion')
+
+    def test_tag_is_not_exists(self):
+        with self.assertRaises(Tag.DoesNotExist):
+            Tag.objects.get(name='123')
