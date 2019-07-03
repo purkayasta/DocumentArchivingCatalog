d14518ae14fbcdce4316970f521c25e8da5c81a2
purkayasta <pritompurkayasta@gmail.com>
2019-07-03 18:52:02 +0600

    Homepage Design Partial Complete

--- catalog/views.py
+++ catalog/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from .forms import SearchForm
+
+
 def homepage(request):
-    return render(request, 'catalog/homepage.html', {})
+
+    if request.method == 'GET':
+        search_form = SearchForm()
+        return render(request, 'catalog/homepage.html', {
+            'form': search_form
+        })
+    else:
+        search_form = SearchForm(request.POST)
+        if search_form.is_valid():
+            content = search_form.cleaned_data['search_content']
+            radio_button = search_form.cleaned_data['selected_radio']
+            print(content)
+            print(radio_button)
