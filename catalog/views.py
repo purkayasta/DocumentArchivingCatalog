ab48856d932f66ec2767ccd3b773886ae34569c5
purkayasta <pritompurkayasta@gmail.com>
2019-07-04 22:23:20 +0600

    Searching Done

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db.models import Q
 
 
 from .forms import SearchForm, DocumentUploadForm
@@ -10,18 +11,35 @@ def homepage(request):
 
     if request.method == 'GET':
         search_form = SearchForm()
-        document_objects = Document.objects.all()
-        return render(request, 'catalog/homepage.html', {
-            'form': search_form,
-            'documents': document_objects
-        })
+        document_objects = Document.objects.all().order_by('-created_at')
+
     else:
         search_form = SearchForm(request.POST)
         if search_form.is_valid():
             content = search_form.cleaned_data['search_content']
             radio_button = search_form.cleaned_data['selected_radio']
-            print(content)
-            print(radio_button)
+            if (content is not None or content != ''):
+                # print(content)
+                # print(radio_button)
+                if (radio_button == 3):
+                    document_objects = Document.objects.filter(
+                        Q(keywords__name__icontains=content))
+
+                    # print(document_objects)
+                elif (radio_button == 2):
+
+                    document_objects = Document.objects.filter(
+                        Q(name__icontains=content))
+
+                else:
+                    document_objects = Document.objects.filter(Q(
+                        keywords__name__icontains=content) | Q(
+                        name__icontains=content)).distinct()
+
+    return render(request, 'catalog/homepage.html', {
+        'form': search_form,
+        'documents': document_objects
+    })
 
 
 def document_upload(request):
